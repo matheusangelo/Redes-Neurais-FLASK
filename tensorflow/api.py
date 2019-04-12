2be764e55d7fe3216ed50106654f633369820992
Matheus Silva <matheus.silva@confitec.com.br>
2019-04-12 16:34:56 -0300

    Implementação do cors

--- tensorflow/api.py
+++ tensorflow/api.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, make_response
 from tensorboard import print_function
 
 #implementação do blueprint do tensorflow
@@ -8,9 +8,19 @@ tensorflow_v1 = Blueprint(
     __name__,
     url_prefix="/v1/tensorflow")
 
+
+@tensorflow_v1.route("/", methods=['OPTIONS'])
+def options():
+    response = make_response()
+
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+
+    return response
+
 @tensorflow_v1.route("/")
 def index():
-
     resposta = {"status":200}
 
     return  jsonify(resposta)
