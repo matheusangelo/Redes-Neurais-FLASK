e1408bc4b0c8054acaf81d909971c3dac5e33599
Matheus Silva <matheus.silva@confitec.com.br>
2019-05-02 12:44:06 -0300

    Carga prontuario

--- mongodb/recurso.py
+++ mongodb/recurso.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
+import uuid
 # from mock import mock_pacientes
+
+id_carga = str(uuid.uuid4())
 
 
 class Carga:
@@ -8,42 +11,49 @@ class Carga:
 
         carga_pacientes = [
             {
+                "id": id_carga,
                 "nome": "Matheus",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Marcos",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Lucas",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Judas",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Thiago",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Pedro",
                 "idade": "27",
                 "prioridade": "Alta",
                 "status": "Não Processado"
             },
             {
+                "id": str(uuid.uuid4()),
                 "nome": "Paulo",
                 "idade": "27",
                 "prioridade": "Alta",
@@ -52,89 +62,82 @@ class Carga:
         ]
 
         # conexao com o banco
-        cliente = MongoClient('localhost', 17017)
+        cliente=MongoClient('localhost', 17017)
 
         # selecionando o banco
-        banco = cliente['tcc']
+        banco=cliente['tcc']
 
         # acessando a respectiva tabela
-        prontuario = banco['pacientes']
+        prontuario=banco['pacientes']
 
-        paciente_id = prontuario.insert_many(carga_pacientes)
+        paciente_id=prontuario.insert_many(carga_pacientes)
 
         print("ok")
 
     def carga_prontuario(self):
 
-        carga_prontuarios = [
+        carga_prontuarios=[
             {
-                "id": "1",
-                "nome": "Matheus",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "2",
-                "nome": "Marcos",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "3",
-                "nome": "Lucas",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "4",
-                "nome": "Judas",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "5",
-                "nome": "Thiago",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "6",
-                "nome": "Pedro",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
-            {
-                "id": "7",
-                "nome": "Paulo",
-                "idade": "27",
-                "prioridade": "Alta",
-                "status": "OK"
-            },
+                "prontuario": {
+                    "id_paciente": id_carga,
+                    "nome": "teste",
+                    "idade": 1,
+                    "observacoes": "ABCDEFG",
+                    "sintomas": [
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 0
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 1
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 2
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 3
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 4
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 5
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 5
+                        },
+                        {
+                            "sintoma": "XPTO",
+                            "grau": 5
+                        }
+                    ]
+                }
+            }
         ]
 
         # conexao com o banco
-        cliente = MongoClient('localhost', 17017)
+        cliente=MongoClient('localhost', 17017)
 
         # selecionando o banco
-        banco = cliente['tcc']
+        banco=cliente['tcc']
 
         # acessando a respectiva tabela
-        prontuario = banco['prontuarios']
+        prontuario=banco['prontuarios']
 
-        prontuario_id = prontuario.insert_many(carga_prontuarios)
+        prontuario_id=prontuario.insert_many(carga_prontuarios)
 
         print("ok")
 
     def carga_doencas(self):
 
         # json com as doenças
-        carga_doencas = [
+        carga_doencas=[
             {
                 "id": "1",
                 "nome": "Matheus",
@@ -187,14 +190,14 @@ class Carga:
         ]
 
         # conexao com o banco
-        cliente = MongoClient('localhost', 17017)
+        cliente=MongoClient('localhost', 17017)
 
         # selecionando o banco
-        banco = cliente['tcc']
+        banco=cliente['tcc']
 
         # acessando a respectiva tabela
-        doenca = banco['doencas']
+        doenca=banco['doencas']
 
-        doenca_id = prontuario.insert_many(carga_doencas)
+        doenca_id=prontuario.insert_many(carga_doencas)
 
         print("ok")
