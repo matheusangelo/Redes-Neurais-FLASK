5d3e877ecacbc7b475f5eb4dd7a97302214abd0d
Matheus Silva <msilva@brasilseg.com.br>
2019-07-17 12:18:21 -0300

    CRUD

--- pacientes/api.py
+++ pacientes/api.py
@@ -14,3 +14,39 @@ def index_v1():
     lista_pacientes = lista_pacientes.process()
 
     return lista_pacientes
+
+
+@pacientes_v1.route("/", methods=["POST"])
+def retornar_pacientes():
+
+    lista_pacientes = ListarPacientes()
+    lista_pacientes = lista_pacientes.process()
+
+    return lista_pacientes
+
+
+@pacientes_v1.route("/", methods=["PUT"])
+def criar_pacientes():
+
+    lista_pacientes = ListarPacientes()
+    lista_pacientes = lista_pacientes.process()
+
+    return lista_pacientes
+
+
+@pacientes_v1.route("/", methods=["DELETE"])
+def deletar_pacientes():
+
+    lista_pacientes = ListarPacientes()
+    lista_pacientes = lista_pacientes.process()
+
+    return 200
+
+
+@pacientes_v1.route("/", methods=["GET"])
+def retornar_paciente_por_id():
+
+    lista_pacientes = ListarPacientes()
+    lista_pacientes = lista_pacientes.process()
+
+    return lista_pacientes
